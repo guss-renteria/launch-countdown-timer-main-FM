eb43b2d35df4426d0d82863b93dea9af61d36556
guss-renteria <gusstavo.renteria.g@gmail.com>
2022-06-15 13:59:05 -0500

    se configuro la estructura estatica

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 
 import './normalize.css'
 import './main.scss'
+import './icons/icomoon/style.css'
 
 import router from './router/router'
 
--- src/views/home/home.view.vue
+++ src/views/home/home.view.vue
@@ -1,18 +1,36 @@
 <script>
 import timer from '@/components/timer/timer.component.vue'
+import image_bg from '@/images/bg-stars.svg'
+import image_hills from '@/images/pattern-hills.svg'
 
 export default {
+  data: () => ({
+  }),
+
   components: {
     timer,
   },
+
+  mounted() {
+    console.log(this.$refs.home.style)
+    this.$refs.home.style.backgroundImage = `url(${ image_bg })`
+    this.$refs.footer.style.backgroundImage = `url(${ image_hills })`
+  }
 }
 </script>
 
 
 <template>
-  <section class='home-view'>
-    <h1>We're launching soon</h1>
-    <timer />
+  <section class='home-view' ref='home'>
+    <div class='home-container'>
+      <h1>We're launching soon</h1>
+      <timer />
+    </div>
+    <div class='home-footer' ref='footer'>
+      <a href='#' class='icon-facebook'></a>
+      <a href='#' class='icon-pinterest'></a>
+      <a href='#' class='icon-instagram'></a>
+    </div>
   </section>
 </template>
 
